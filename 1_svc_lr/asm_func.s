78e5341e692831b0beecff3b33f2b1e7a0bcee4f
P86071244 <P86071244@gs.ncku.edu.tw>
2019-06-12 01:35:29 +0800

    1

--- 1_svc_lr/asm_func.s
+++ 1_svc_lr/asm_func.s
@@ -33,9 +33,11 @@ start_user:
 
 .global	sys_call
 sys_call:
-	??????
+	svc	#0x3 ; // Call SVC function 3
+	bx	lr
 
 .type svc_handler, %function
 .global svc_handler
 svc_handler:
-	??????
+	movs	r0,	lr
+	b svc_handler_c
